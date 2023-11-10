7efcf18766fa4a6ec084d97f91fe1dedf2a2cbde
Manas Bisht <88678715+GreyHatStyle@users.noreply.github.com>
2023-11-11 05:29:40 +0530

    Update TeleBot.py

--- TeleBot.py
+++ TeleBot.py
@@ -1,51 +1,59 @@
 # Passed!!
 from typing import Final
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-
+import time
+import Extras
+# from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import telegram.ext as tl
 Token: Final = "6461982132:AAFTBNN4DqEb-hG6gxoY7zyr83CJ88JVqgk"
 Bot_username: Final = '@manas_hos_bot'
 
 
 # Commands
 
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello please Write your complain in (Room no. : Complain field) format\nEg: B-04 "
-                                    ": Plumber")
+async def start_command(update: Update, context: tl.ContextTypes.DEFAULT_TYPE):
+    s = """Hello! Please write your complain (For Plumber, Carpenter or Electrician) 
+in this Format (A-101 : Plumber)\n
+If you have Complain related to other issues then follow this format
+(A-101 : Others : {Your Name} : {Your Complain})..."""
+    await update.message.reply_text(s)
 
 
 # Responses
 
-def handle_response(text: str) -> str:
-    if ':' in text:
-        return "Complain Registered!"
+def handle_response(text: str, date) -> str:
+    check = False
+    lst = list(text.split(":"))
+    check = Extras.check_Message(lst, str(date))
+
+    if check:
+        return "Complain Registered"
     else:
-        return "Incomplete information"
+        return "Please Type complain in mentioned Format"
 
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def handle_message(update: Update, context: tl.ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
-
-    print(f'User ({update.message.chat.id} in {message_type}: [{text}]')
-
-    response: str = handle_response(text)
+    data_message = f'User ({update.message.chat.id} in {message_type}: [{text}] date : [{update.message.date}]'
+    date = (update.message.date).date()
+    response: str = handle_response(text, date)
     print("Bot: ", response)
     await update.message.reply_text(response)
 
 
-async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def error(update: Update, context: tl.ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused the error {context.error}')
 
 
-print("Starting bot...")
-app = Application.builder().token(Token).build()
-app.add_handler(CommandHandler('start', start_command))
+def main():
+    print("Starting bot...")
 
-# Messages
-app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app = tl.Application.builder().token(Token).build()
+    app.add_handler(tl.CommandHandler('start', start_command))
 
-# Error
-app.add_error_handler(error)
-print("Polling...")
-app.run_polling(poll_interval=3)
+    # Messages
+    app.add_handler(tl.MessageHandler(tl.filters.TEXT, handle_message))
+    # Error
+    app.add_error_handler(error)
+    app.run_polling(poll_interval=3)
