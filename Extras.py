f40c1106da7e25df3e081cf5e75d5447f3615647
Manas Bisht <88678715+GreyHatStyle@users.noreply.github.com>
2023-11-11 05:28:59 +0530

    Update Extras.py

--- Extras.py
+++ Extras.py
@@ -6,7 +6,7 @@ def RoomData():
     lst = baseData.Block_check()
     s = "Empty Room and Number of seats Available: \n\n"
 
-    if len(lst)==0:
+    if len(lst) == 0:
         messagebox.showinfo('Information', 'All rooms are filled!')
         return
     count = 1
@@ -54,3 +54,59 @@ def Information_Provide(lst):
     s += f"Father Name: {lst[0][5]}\nFather Number: {lst[0][6]}"
 
     messagebox.showinfo(f"Data of {lst[0][2]}", s)
+
+# Telegram Functions
+
+
+def Give_List_of_Complains(name: str) -> list:
+    f = open(f"Data\\{name}.txt", "r")
+    ans = []
+    lst = list(f.read().split('\n'))
+    lst2 = list(lst[0].split(','))
+
+    for data in lst:
+        l = list(data.split(','))
+        try:
+            ans.append(l[1])
+        except Exception as e:
+            print(f"[Exception during list creation ]: {e}")
+
+    return ans
+
+
+
+def check_Message(lst: list, date) -> bool:
+    dr_lst = ['plumber', 'carpenter', 'electrician', 'others']
+    if lst[1].lower() in dr_lst:
+
+        f = open(f"Data\\{lst[1].lower()}.txt", "a")
+        lst.insert(0, date)
+
+        for s in lst:
+            f.write(f"{s}, ")
+
+        f.write("\n")
+        f.close()
+        return True
+    else:
+        return False
+
+
+def Give_List_of_Others() -> list:
+    f = open(f"Data\\others.txt", "r")
+    ans = []
+    lst = list(f.read().split('\n'))
+
+    for data in lst:
+        try:
+            l = list(data.split(','))
+            t = []
+            t.append(l[1])
+            t.append(l[3])
+            t.append(l[4])
+            ans.append(t)
+        except:
+            pass
+
+    return ans
+
