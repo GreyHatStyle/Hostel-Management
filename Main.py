b280c4993262f90941f5402cb9f5a34b0f65412c
Manas Bisht <88678715+GreyHatStyle@users.noreply.github.com>
2023-11-11 05:30:57 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+import baseGUI
+import TeleBot
+import multiprocessing
+
+p1 = multiprocessing.Process(target=baseGUI.head_gui)
+p2 = multiprocessing.Process(target=TeleBot.main)
+
+if __name__ == '__main__':
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+print("done")
